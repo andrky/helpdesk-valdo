82b0df2ee7dece6466b1ec1bc2576ba61cec41c0
M. Ananda Rizky Audriansyah <anandarizky166@gmail.com>
2022-11-19 15:42:57 +0700

    Modified User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,10 +15,12 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'karyawan_id' => mt_rand(1,20),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'username' => $this->faker->username(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'level' => $this->faker->words(1,true),
+            'email_verified_at' => now(),
             'remember_token' => Str::random(10),
         ];
     }
